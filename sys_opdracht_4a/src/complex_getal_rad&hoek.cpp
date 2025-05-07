71690fdb8e6fb727a02056af34f8c46aff382d1c
Rob-Hva <rob.beaufort.beaufort@hva.nl>
2025-05-07 13:15:57 +0200

    commentaar toegevoegd aan 4a
    Ik was vergeten commentaar toe te voegen aan 4a.

--- sys_opdracht_4a/src/complex_getal_rad&hoek.cpp
+++ sys_opdracht_4a/src/complex_getal_rad&hoek.cpp
@@ -1,8 +1,17 @@
+/**
+ * @file    complex_getal_radENhoek.cpp
+ * @author  Rob Beaufort
+ * @brief   Er kan een complex getal aangemaakt worden. Dit getal kan je een radius en een hoek meegeven.
+ * @version 1.0
+ * @date    7-5-2025
+ */
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
+// Dit is de class Complex. hier wordt een complex getal in opgeslagen.
 class Complex{
     public:
         Complex(int hoek, int radius);
@@ -20,6 +29,7 @@ Complex::Complex(int hoek, int radius){
 
 }
 
+// Hier worden de waardes van het complexe getal geprint.
 void Complex::drukAf(){
     cout << "De hoek is: " << hoe << endl;
     cout << "De radius is: " << rad << endl;
@@ -30,13 +40,15 @@ int main(){
     int hoeInput;
     int radInput;
 
+    // Hier kiest de gebruiker de waardes voor complex getal 1.
     cout << "De hoek getal 1: ";
     cin >> hoeInput;
     cout << "De radius getal 1: ";
     cin >> radInput;
 
     Complex c1 = *new  Complex(hoeInput, radInput);
-
+    
+    // Hier kiest de gebruiker de waardes voor complex getal 2.
     cout << "De hoek getal 2: ";
     cin >> hoeInput;
     cout << "De radius getal 2: ";
